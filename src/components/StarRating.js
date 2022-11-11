c70bbf55b1e7ebfa34afb0b75e3da5e86a311322
David Kim <cjoak1028@gmail.com>
2022-11-11 10:16:04 -0500

    Added star rating component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import "../css/StarRating.css";
 
 const StarRating = () => {
-  return <div>{[...Array(5)].fill(<FaStar size={20} />)}</div>;
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label>
+            <input
+              className="star-input"
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              size={20}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
 };
 
 export default StarRating;
