23ff65928128aea4e51259b38739c02ba49d43a7
David Kim <cjoak1028@gmail.com>
2022-11-11 10:39:25 -0500

    Started on Review List

--- src/components/ReviewSection.js
+++ src/components/ReviewSection.js
@@ -1,11 +1,34 @@
 import React from "react";
 import "../css/ReviewSection.css";
 import ReviewForm from "./ReviewForm";
+import ReviewsList from "./ReviewsList";
+
+const SAMPLE_DATA = [
+  {
+    first: "Michael",
+    last: "Hong",
+    review: "I hate this course!",
+    rating: 4,
+  },
+  {
+    first: "David",
+    last: "Kim",
+    review: "I love this course!",
+    rating: 3,
+  },
+  {
+    first: "Jenny",
+    last: "Kim",
+    review: "I failed this course!",
+    rating: 2,
+  },
+];
 
 const ReviewSection = () => {
   return (
     <div className="review-section">
       <ReviewForm />
+      <ReviewsList data={SAMPLE_DATA} />
     </div>
   );
 };
